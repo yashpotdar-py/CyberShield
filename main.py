4f7f5485aeeb49aca3084bc49c67b2133ecd574b
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-05-22 19:47:04 +0530

    Minor changes to main.kv and main.py

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class CyberShieldApp(MDApp):
     }
     def build(self):
         return Builder.load_file("main.kv")
+    
+    def login_data(self):
+        website = self.screen_manager.password.website.text
+        username = self.username.text
+        password = self.password.text
+
+        print(website)
+        print(username)
+        print(password)
 
 
 if __name__ == "__main__":
