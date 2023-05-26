99b18c55b244942178c57466913d1f9838c41216
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-05-26 19:15:45 +0530

    Modifying Password Manager

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-
+from kivy.properties import ObjectProperty
 
 class CyberShieldApp(MDApp):
-
-    data = {
-        "Application" : "android",
-        "Website" : "web"
-    }
+    screen_manager  = ObjectProperty(None)
     def build(self):
-        return Builder.load_file("main.kv")
+        self.screen = Builder.load_file("main.kv")
+        return self.screen
     
     def login_data(self):
-        website = self.screen_manager.password.website.text
-        username = self.username.text
-        password = self.password.text
+        website = self.screen.screen_manager.passwordmanager.website.text
+        username = self.screen.screen_manager.passwordmanager.username.text
+        password = self.screen.screen_manager.passwordmanager.password.text
 
         print(website)
         print(username)
