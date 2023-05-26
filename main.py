b0c5ab32a6cd25aa04f7973816aa0c853350474e
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-05-26 20:18:39 +0530

    Adding settings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.config import Config
+from kivymd.uix.pickers import MDDatePicker
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 class CyberShieldApp(MDApp):
@@ -9,6 +10,11 @@ class CyberShieldApp(MDApp):
     def build(self):
         self.screen = Builder.load_file("main.kv")
         return self.screen
+    
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_pause=self.on_pause)
+        date_dialog.open()
     
     def login_data(self):
         website = self.screen.screen_manager.passwordmanager.website.text
