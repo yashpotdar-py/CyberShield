5d33287ba2cd9ab759245bfe56a6b50bd0a3a608
yashpotdar-py <yashyogeshpotdar7@gmail.com>
2023-06-05 21:40:05 +0530

    .

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class CyberShieldApp(MDApp):
     screen_manager = ObjectProperty(None)
 
     def build(self):
+        self.theme_cls.material_style = "M3"
         self.theme_cls.theme_style_switch_animation = True
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Purple"
